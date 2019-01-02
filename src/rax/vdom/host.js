e10884781e1e10b668bb897482cb96654719db93
waisuikei <wei.shaoji@myquant.com>
2019-01-02 09:48:51 +0800

    wip

--- src/rax/vdom/host.js
+++ src/rax/vdom/host.js
@@ -1,9 +1,3 @@
-import EmptyComponent from './empty';
-import NativeComponent from './native';
-import TextComponent from './text';
-import CompositeComponent from './composite';
-import FragmentComponent from './fragment';
-
 export default {
   component: null,
   mountID: 1,
@@ -11,13 +5,4 @@ export default {
   // Roots
   rootComponents: {},
   rootInstances: {},
-  // Inject
-  hook: null,
-  driver: null,
-  monitor: null,
-  EmptyComponent,
-  NativeComponent,
-  TextComponent,
-  FragmentComponent,
-  CompositeComponent
 };
--- src/rax/vdom/instantiateComponent.js
+++ src/rax/vdom/instantiateComponent.js
@@ -1,21 +1,25 @@
-import Host from './host';
+import EmptyComponent from './empty';
+import FragmentComponent from './fragment';
+import NativeComponent from './native';
+import CompositeComponent from './composite';
+import TextComponent from './text';
 
 function instantiateComponent(element) {
   let instance;
 
   if (element === undefined || element === null || element === false || element === true) {
-    instance = new Host.EmptyComponent();
+    instance = new EmptyComponent();
   } else if (Array.isArray(element)) {
-    instance = new Host.FragmentComponent(element);
+    instance = new FragmentComponent(element);
   } else if (typeof element === 'object' && element.type) {
     // Special case string values
     if (typeof element.type === 'string') {
-      instance = new Host.NativeComponent(element);
+      instance = new NativeComponent(element);
     } else {
-      instance = new Host.CompositeComponent(element);
+      instance = new CompositeComponent(element);
     }
   } else if (typeof element === 'string' || typeof element === 'number') {
-    instance = new Host.TextComponent(element);
+    instance = new TextComponent(element);
   } else {
     throw new Error(`Invalid element type: ${element}. (current: ${typeof element === 'object' && Object.keys(element) || typeof element})`);
   }
