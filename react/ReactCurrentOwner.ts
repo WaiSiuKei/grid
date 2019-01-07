0e8f6ab2e00a77ab297ff074265e00b20524a53d
waisuikei <wei.shaoji@myquant.com>
2019-01-07 15:34:50 +0800

    wip

--- react/ReactCurrentOwner.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'src/react/def';
-import { DispatcherWithoutHooks } from 'src/react/ReactFiberDispatcher';
-
-const currentDispatcher = DispatcherWithoutHooks;
-const current: ReactElement<any> = null;
-export const ReactCurrentOwner = {
-  current,
-  currentDispatcher
-};
--- react/ReactDOMHostConfig.ts
+++
@@ -1,7 +0,0 @@
-import { DOMContainer } from 'src/react/ReactDOM';
-
-export function prepareForCommit(containerInfo: DOMContainer): void {
-  eventsEnabled = ReactBrowserEventEmitterIsEnabled();
-  selectionInformation = getSelectionInformation();
-  ReactBrowserEventEmitterSetEnabled(false);
-}
