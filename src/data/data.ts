799a02f7b65aac5f05ae65f07ceb3ef94167e1bd
waisuikei <wei.shaoji@myquant.com>
2019-02-27 15:22:54 +0800

    wip

--- src/data/data.ts
+++ src/data/data.ts
@@ -11,11 +11,6 @@ export interface IDataView {
   scheduleUpdate(): void
 }
 
-export enum DataViewRowsChangeReason {
-  Add = 'add',
-  Remove = 'remove'
-}
-
 export type IDataSet = Datum[] | IDataView
 
 export function defaultGroupFormatter(group: Group, setting: GroupingSetting): any {
@@ -24,12 +19,12 @@ export function defaultGroupFormatter(group: Group, setting: GroupingSetting): a
   };
 }
 
-// function defaultGroupTotalFormatter(value: any, rows: Datum[]): any {
-//   return function () {
-//
-//     return React.createElement('div', null, (value + '').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
-//   };
-// }
+function defaultGroupTotalFormatter(key: string, value: any, totals: GroupTotals, setting: GroupingSetting): any {
+  return function () {
+
+    return React.createElement('div', null, `totals: ${value}`);
+  };
+}
 
 export interface Formatter<D, C> {
   (group: D, setting: C): Function
@@ -43,11 +38,12 @@ interface Grouping<T> {
   accessor: T
   comparer: (a: any, b: any) => number,
   formatter: Formatter<Group, GroupingSetting>
-  displayTotalsRow: boolean
+  // aggregators
 }
 
 export interface InternalGroupingSetting extends Grouping<DataAccessor> {
   level: number
+  displayTotalsRow: boolean
 }
 
 export type GroupingSetting = Grouping<string | DataAccessor>
@@ -82,25 +78,29 @@ export interface SortingSetting {
   priority: number
 }
 
-export class NonDataItem {
+export class GroupTotals {
+  key: string;
+  value: string;
 
+  constructor(private group: Group) {
+  }
+
+  get rows() {
+    return this.group.rows;
+  }
 }
 
-export class GroupTotals extends NonDataItem {
-  parent: Group;
-}
-
-export class Group extends NonDataItem {
+export class Group {
   level: number;
   rows: Datum[] = [];
   _collapsed: boolean = false;
   totals: GroupTotals;
   subGroups: Group[];
-  key: any;
+  key: any; // any used as the key of a Map
+  label: string; // label for the key
   $$hash: string;
 
   constructor(private ds: IDataView) {
-    super();
   }
 
   get collapsed() {
